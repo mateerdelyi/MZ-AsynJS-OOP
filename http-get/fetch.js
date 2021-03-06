75b5317ca5ba49a68d9d6372103c6c489dd58c8b
Mate Erdelyi <mate.erdelyi@icloud.com>
2021-03-06 14:47:23 +0100

    error handling done

--- http-get/fetch.js
+++ http-get/fetch.js
@@ -2,8 +2,12 @@ document.getElementById("getText").addEventListener("click", getText);
 
 function getText() {
     fetch("https://jsonplaceholder.typicode.com/users")
-        .then((res) => res.text())
+        .then(response => response.text())
         .then((data) => console.log(data))
+        .catch(error => {
+            console.error(error);
+            alert('Error :(');
+        })
 }
 
 
